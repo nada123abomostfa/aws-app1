0ae047266fd4dae0cc516526cce48b5ca80029e5
nada123abomostfa <101100406+nada123abomostfa@users.noreply.github.com>
2023-09-20 19:48:56 +0300

    first commit1

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
  app.use(express.json());
 
  app.use('/',(req,res) => {
-    return res.status(200).json({message: "hello from AWS Pipeline"})
+    return res.status(200).json({message: "hello from AWS Pipeline and new "})
  })
 
 app.listen(3001);
